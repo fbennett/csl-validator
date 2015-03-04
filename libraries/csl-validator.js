3aa0e3ee0c8db6aaa4a7d0ae1e65706c0a5b3b2a
Frank Bennett <biercenator@gmail.com>
2015-03-04 20:19:41 +0900

    Show error in Source

--- libraries/csl-validator.js
+++ libraries/csl-validator.js
@@ -245,7 +245,7 @@ var CSLValidator = (function() {
                     lineText = "Line " + lastLine;
                 }
                 sourceHighlightRange = firstLine + ',' + firstColumn + ',' + lastLine + ',' + lastColumn;
-                results += '<a style="text-decoration:none;padding:0.25em;border-radius:0.5em;border:1px solid black;" href="#source-code" onclick="CSLValidator.moveToLine(' + sourceHighlightRange + ');">' + lineText + '</a>: ';
+                results += '<a style="text-decoration:none;padding:0.25em;border-radius:0.5em;border:1px solid black;" href="#source-code" onclick="CSLValidator.moveToLine(event,' + sourceHighlightRange + ');">' + lineText + '</a>: ';
 
                 results += messages[i].message;
                 results += "</li>";
@@ -302,8 +302,18 @@ var CSLValidator = (function() {
         saveButton.enable();
     }
 
-    function moveToLine(firstLine, firstColumn, lastLine, lastColumn) {
+    function moveToLine(event,firstLine, firstColumn, lastLine, lastColumn) {
         $("#source-tab").click();
+        $("#error-banner").remove();
+        var errorNode = $('<div id="error-banner" class="inserted" style="display:inline;margin-left:1em;background:#white;border-radius:0.5em;border:1px solid #aaaaaa;padding:0.33em;"><span style="font-weight:bold;">ERROR @ </span><span>').get(0);
+        var infoNode = event.target.parentNode.cloneNode(true);
+        lineNumber = infoNode.firstChild;
+        lineNumber.removeAttribute('onclick');
+        lineNumber.setAttribute('style', 'color:white;font-weight:bold;text-size:smaller;border:none;');
+        errorNode.appendChild(infoNode.firstChild);
+        errorNode.appendChild(infoNode.lastChild);
+        $("#source h4.panel-title").attr('style', 'display:inline;').after(errorNode);
+
         editor.scrollToLine(firstLine, true, true, function() {});
         editor.gotoLine(firstLine, 0, false);
         //alert(firstLine + "," + firstColumn + "," + lastLine + "," + lastColumn);
